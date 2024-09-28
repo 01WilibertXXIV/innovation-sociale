7152c56a232bded95ce757aabf76281fec7647ae
WilibertXXIV <nicolas@hub01.org>
2024-09-28 13:32:43 -0400

    fixed mobile text size

--- src/app/components/webcam/webcam-component.tsx
+++ src/app/components/webcam/webcam-component.tsx
@@ -75,18 +75,18 @@ export default function WebcamComponent(props: { maxFaces: number }) {
   
     return (
       <div>
-        <div className="z-[9999] text-2xl font-bold absolute top-0 left-0 p-6 text-white w-full">
-            <h1 className="text-4xl font-bold ">Innovation sociale P.O.C</h1>
-            <h3 className="text-2xl font-bold   text-white">Number of Faces Detected: {numFaces}</h3>
-            <p className='text-sm'>Pour mon cher Olivier Ross</p>
+        <div className="z-[9999] md:text-2xl font-bold absolute top-0 left-0 p-6 text-white w-full">
+            <h1 className="md:text-4xl font-bold ">Innovation sociale P.O.C</h1>
+            <h3 className="md:text-2xl font-bold   text-white">Nombre de visages détectés: {numFaces}</h3>
+            
         </div>
-
+        <p className='absolute bottom-0 right-0 text-xs p-2'>Pour mon cher Olivier Ross</p>
 
         <div className={`text-4xl font-bold absolute h-full w-full z-[1000] flex justify-center items-center transition-all duration-200 bg-black ${numFaces >= props.maxFaces ? 'opacity-100' : 'opacity-0'}`}>
             <h1 className={`${numFaces >= 1 ? 'text-opacity-100' : 'text-opacity-0'}`}>C'est le temps d'aller parler à quelqu'un</h1>
         </div>
         
-        <video ref={videoRef} autoPlay playsInline className='w-full h-full object-cover'/>
+        <video ref={videoRef} autoPlay playsInline className='object-cover'/>
       </div>
     );
 }
