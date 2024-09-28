a88ae0f68c476950e1dcc5e0f9cb745531efc12f
WilibertXXIV <nicolas@hub01.org>
2024-09-28 13:15:29 -0400

    removed fs and encoding from face-api.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+          // Replace fs and encoding with empty modules when building for the browser
+          config.resolve.fallback = {
+            fs: false,
+            encoding: false,
+          };
+        }
+        return config;
+      },
+};
 
 export default nextConfig;
